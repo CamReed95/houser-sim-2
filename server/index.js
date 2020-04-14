1fce5feb85535a3ff7296b149b14e760f0e1397f
Cameron Reed <cameronreedadjusting@gmail.com>
2020-04-13 19:27:27 -0700

    Made some progress 4/13

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
-// const session = require('express-session');
+const session = require('express-session');
 const controller = require('./controller');
 const massive = require('massive');
+const app = express()
+require('dotenv').config()
 
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
 
-const app = express();
+massive ({
+    connectionString:CONNECTION_STRING,
+    ssl: {rejectAunauthorized: false}
+}).then(db => {
+    app.set('db', db)
+    console.log('db connected')
+}).catch(err => console.log('cannot connect to db', err))
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false, 
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 14
+    }
+}))
 
 app.use(express.json());
 
